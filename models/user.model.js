b97d879d967bc1704e5034db8a6de0cbb978588f
IbnuAhsani <ibnu.muhari@gmail.com>
2019-11-16 17:23:17 +0700

    feat: set id attribute in user model to unique

--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  id: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
   first_name: { type: String, required: true, max: 100 },
   family_name: { type: String, required: true, max: 100 }
 });
