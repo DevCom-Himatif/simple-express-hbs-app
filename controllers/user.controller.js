1651b6aa6b2a31cd21395d436a999f911fcafd7e
IbnuAhsani <ibnu.muhari@gmail.com>
2019-11-16 17:23:47 +0700

    feat: get one & update user based on id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,6 @@
 const UserModel = require("../models/user.model");
 const uuidv1 = require("uuid/v1");
 
-const getAll = async (req, res) => {
-  const users = await UserModel.find().exec();
-
-  if (users.length === 0) {
-    return res.status(200).send({ message: "No user has signed up" });
-  }
-
-  return res.status(200).send(users);
-};
-
 const create = (req, res) => {
   const { body } = req;
 
@@ -28,4 +18,57 @@ const create = (req, res) => {
     .catch(error => res.status(500).send({ message: error.message }));
 };
 
-module.exports = { getAll, create };
+const getAll = async (req, res) => {
+  const users = await UserModel.find().exec();
+
+  if (users.length === 0)
+    return res.status(400).send({ message: "No user has signed up" });
+
+  return res.status(200).send(users);
+};
+
+const getOne = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserModel.find({ id });
+
+  if (user.length === 0)
+    return res
+      .status(400)
+      .send({ message: `User with id: ${id} doesn't exists` });
+
+  return res.status(200).send(user);
+};
+
+const update = async (req, res) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+
+  const updateResult = await UserModel.updateOne({ id }, body);
+  const {
+    n: numUserFound,
+    nModified: numModifiedUser,
+    ok: isUpdateSuccessful
+  } = updateResult;
+
+  if (numUserFound !== 1)
+    return res
+      .status(400)
+      .send({ message: `User with id: ${id} doesn't exists` });
+
+  if (numModifiedUser < 1)
+    return res
+      .status(400)
+      .send({ message: `Data that you input is the same as the old data` });
+
+  if (!isUpdateSuccessful || numModifiedUser < 1)
+    return res
+      .status(400)
+      .send({ message: `Failed to update user with id: ${id}` });
+
+  return res.status(200).send(updateResult);
+};
+
+module.exports = { create, getAll, getOne, update };
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get("/ping", (req, res) => res.status(200).json("pong"));
 
 router.get("/authors", userController.getAll);
+router.get("/author/:id", userController.getOne);
 router.post("/author", userController.create);
+router.put("/author/:id", userController.update);
 
 module.exports = router;
